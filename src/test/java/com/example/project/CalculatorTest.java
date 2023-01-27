51bf6188b1637406e648a3228d77a8f64bdc25c4
Jing Zhang <sherry.jing@hotmail.com>
2023-01-27 11:35:16 +0100

    Added substract and multiply

--- src/test/java/com/example/project/CalculatorTest.java
+++ src/test/java/com/example/project/CalculatorTest.java
@@ -28,4 +28,43 @@ public class CalculatorTest {
         assertEquals(expectedResult, calculator.add(first, second),
                 () -> first + " + " + second + " should equal " + expectedResult);
     }
+
+    @Test
+    @DisplayName("1 - 1 = 0")
+    void subtractTwoNumbers(){
+        Calculator calculator = new Calculator();
+        assertEquals(0, calculator.substract(1,1), "1 - 1 should equal 0");
+    }
+    @ParameterizedTest(name = "{0} - {1} = {2}")
+    @CsvSource({
+            "2,    -2,   4",
+            "3,    1,   2",
+            "50,  2, 48",
+            "101,  100, 1"
+    })
+    void substract(int first, int second, int expectedResult){
+        Calculator calculator = new Calculator();
+        assertEquals(expectedResult, calculator.substract(first, second),
+                () -> first + " - " + second + "should equal " + expectedResult);
+    }
+
+    @Test
+    @DisplayName("1 * 1 = 1")
+    void multiplyTwoNumbers(){
+        Calculator calculator = new Calculator();
+        assertEquals(1, calculator.multiply(1,1), "1 * 1 should equal 1");
+    }
+    @ParameterizedTest(name = "{0} * {1} = {2}")
+    @CsvSource({
+            "2,    -1,   -2",
+            "3,    3,   9",
+            "50,  2, 100",
+            "100,  3, 300"
+    })
+    void multiply(int first, int second, int expectedResult){
+        Calculator calculator = new Calculator();
+        assertEquals(expectedResult, calculator.multiply(first, second),
+                () -> first + " * " + second + "should equal " + expectedResult);
+    }
+
 }
