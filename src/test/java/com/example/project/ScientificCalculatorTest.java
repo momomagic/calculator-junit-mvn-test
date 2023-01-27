d1c6db5964a67489b4fc90ed7bcb1b0c685c1525
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-01-27 12:38:50 +0100

    Started with test

--- src/test/java/com/example/project/ScientificCalculatorTest.java
+++ src/test/java/com/example/project/ScientificCalculatorTest.java
@@ -1,11 +1,26 @@
 package com.example.project;
 
 import com.example.project.http.Connector;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 public class ScientificCalculatorTest {
-    @Mock private Connector connector;
+    @Mock
+    private Connector connector;
+
+    @Test
+    void authorizeConnection(){
+        ScientificCalculator scientificCalculator = new ScientificCalculator(connector, "http://dummy");
+
+        when(connector.connect("http://dummy")).thenReturn(false);
+        Assertions.assertThrowsExactly(RuntimeException.class,()->
+                scientificCalculator.add(5,4));
+
+    }
 }
