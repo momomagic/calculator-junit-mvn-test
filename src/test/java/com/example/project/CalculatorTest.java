78292723210aadbae9dd2c34e30c5902d4c7a8d0
Lilia Bekuzina <lilia.bekuzina1@gmail.com>
2023-01-27 13:28:38 +0100

    modified some files, created new tests

--- src/test/java/com/example/project/CalculatorTest.java
+++ src/test/java/com/example/project/CalculatorTest.java
@@ -9,23 +9,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+    private static final Calculator calculator = new Calculator();
+
     @Test
     @DisplayName("1 + 1 = 2")
     void addsTwoNumbers() {
         Calculator calculator = new Calculator();
         assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+
     }
 
-    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @ParameterizedTest(name = "{0} {3} {1} = {2}")
     @CsvSource({
-            "0,    1,   1",
-            "1,    2,   3",
-            "49,  51, 100",
-            "1,  100, 101"
+            "0,    1,   1, ADD",
+            "1,    2,   3, ADD",
+            "49,  51, 100, ADD",
+            "105,  4, 101, SUBTRACT",
+            "113,  10, 103, SUBTRACT",
+            "15,  7, 8, SUBTRACT",
+            "15,  0, 0, MULTIPLY",
+            "5,  7, 35, MULTIPLY",
+            "15,  7, 105, MULTIPLY",
+            "48,  6, 8, DIVIDE",
+            "35,  7, 5, DIVIDE",
+            "210,  7, 30, DIVIDE"
     })
-    void add(int first, int second, int expectedResult) {
-        Calculator calculator = new Calculator();
-        assertEquals(expectedResult, calculator.add(first, second),
-                () -> first + " + " + second + " should equal " + expectedResult);
+    void testOperation(int first, int second, int expectedResult, String operation) {
+        switch (operation) {
+            case "ADD":
+                assertEquals(expectedResult, calculator.add(first, second));
+                break;
+            case "SUBTRACT":
+                assertEquals(expectedResult, calculator.subtract(first, second));
+                break;
+            case "MULTIPLY":
+                assertEquals(expectedResult, calculator.multiply(first, second));
+                break;
+            case "DIVIDE":
+                assertEquals(expectedResult, calculator.divide(first, second));
+                break;
+        }
     }
 }
+
+
+
--- src/test/java/com/example/project/ScientificCalculatorTest.java
+++ src/test/java/com/example/project/ScientificCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.example.project;
 
 import com.example.project.http.Connector;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -8,4 +9,5 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class ScientificCalculatorTest {
     @Mock private Connector connector;
+
 }
